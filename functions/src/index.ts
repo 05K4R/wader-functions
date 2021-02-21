bfb16cef8c473a0ca782a5b8f4e1aef026862637
Oskar Kärrman <karrman.oskar@gmail.com>
2021-02-21 17:28:42 +0100

    Add TS index importing old JS functions

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import { updateProfile as updateProfileJs } from './js_index.js';
+import { updateTrack as updateTrackJs } from './js_index.js';
+import { updateRepost as updateRepostJs } from './js_index.js';
+import { setCategoryOnTrack as setCategoryOnTrackJs } from './js_index.js';
+import { getProfileScore as getProfileScoreJs } from './js_index.js';
+
+export const updateProfile = updateProfileJs;
+export const updateTrack = updateTrackJs;
+export const updateRepost = updateRepostJs;
+export const setCategoryOnTrack = setCategoryOnTrackJs;
+export const getProfileScore = getProfileScoreJs;
